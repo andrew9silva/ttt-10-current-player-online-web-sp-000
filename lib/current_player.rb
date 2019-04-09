3d85c2689a6e0abb4a5ab2170e8c90410e1e0889
Andrew Silva <andrewsilva9@gmail.com>
2019-04-09 00:54:51 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,9 @@ end
 
 
 def current_player(board)
-  if turn_count(board) % 2
+  if turn_count(board) % 2 == 0
     "O"
-  else turn_count(board) !% 2
+  else turn_count(board) % 2 !== 0
     "X"
   end
 end
